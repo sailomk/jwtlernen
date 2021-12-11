31a780d79568c2b0c5a1789872e78219be8916dc
KiatiphongK <kiatiphongkanokbanakorn@TDG-M0267.local>
2021-12-11 14:50:30 +0700

    add more path

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = process.env.port || 3000
 app.get('/',(req,res) => {
     res.send(' Self Motivation ')
 })
-
+app.get('/me',(req,res) => {
+    res.send(' Hi Cool a bit')
+})
 app.listen(port,() => {
     console.log(`App listening at http://localhost:${port}`)
 })
